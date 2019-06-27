62e9ce67fb08ea7b2f7866c7f4e187ee1968e040
guanyichen <trcgabriella@163.com>
2019-06-27 19:18:05 +0800

    部门管理界面实现:表现层，添加页面 toAdd，add,department_add.jsp

--- sm_web/src/main/java/com/bupt/sm/controller/DepartmentController.java
+++ sm_web/src/main/java/com/bupt/sm/controller/DepartmentController.java
@@ -21,4 +21,19 @@ public class DepartmentController {
         request.setAttribute("LIST",list);//设定LIST键
         request.getRequestDispatcher("../department_list.jsp").forward(request,response);//jsp路径，访问时注意考虑相对路径
     }
+
+    public void toAdd(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("../department_add.jsp").forward(request,response);
+    }
+    public void add(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       String name = request.getParameter("name");
+       String address = request.getParameter("address");
+
+       Department department = new Department();
+       department.setName(name);
+       department.setAddress(address);
+
+       departmentService.add(department);
+       response.sendRedirect("list.do");//重定向会list界面
+    }
 }
